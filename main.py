f118dfa68883f772d32f7937a259797bf6a42196
Rian55 <59274842+Rian55@users.noreply.github.com>
2022-08-08 14:11:56 +0300

    bulk patching demo added

--- main.py
+++ main.py
@@ -12,21 +12,61 @@ listing_us = ListingsItems(credentials=credentials, marketplace=Marketplaces.US)
 types = ProductTypeDefinitions(credentials=credentials, marketplace=Marketplaces.UK)
 # notifications = Notifications(credentials=credentials, marketplace=Marketplaces.UK)
 
-# xdxd = types.get_definitions_product_type(productType="VASE", marketplaceIds=['A1F83G8C2ARO7P'])
+# xdxd = types.get_definitions_product_type(productType="SHOES", marketplaceIds=['A1F83G8C2ARO7P'])
 # print(xdxd)
 # xd = types.search_definitions_product_types(marketplaceIds=['A1F83G8C2ARO7P'])
 # print(xd)
 
-# file = open('test3.json', "r+")
-# body = json.load(file)
-# resp = listing.put_listings_item(sellerId='A2YSV8HF6GQ3SP', sku='EWP-SATURN-CER-2', body=body,
-#                                  marketplaceIds=['A1F83G8C2ARO7P'])
-# print(resp)
-# file.close()
 
-file2 = open('test2.json', "r+")
-body2 = json.load(file2)
-resp = listing.put_listings_item(sellerId='A2YSV8HF6GQ3SP', sku='EWP-SATURN-CER-2', body=body2,
-                                 marketplaceIds=['ATVPDKIKX0DER'])
-print(resp)
-file2.close()
+def add_item_uk():
+    file = open('test3.json', "r+")
+    body = json.load(file)
+    resp = listing.put_listings_item(sellerId='A2YSV8HF6GQ3SP', sku='EWP-SATURN-CER-2', body=body,
+                                     marketplaceIds=['A1F83G8C2ARO7P'])
+    print(resp)
+    file.close()
+
+
+def add_item_us():
+    file2 = open('test2.json', "r+")
+    body2 = json.load(file2)
+    resp = listing.put_listings_item(sellerId='A2YSV8HF6GQ3SP', sku='EWP-SATURN-CER-2', body=body2,
+                                     marketplaceIds=['ATVPDKIKX0DER'])
+    print(resp)
+    file2.close()
+
+
+def patch_uk(sku):
+    text = listing.get_listings_item(sellerId='A2YSV8HF6GQ3SP', sku=sku,
+                                     marketplaceIds=['A1F83G8C2ARO7P']).payload['summaries'][0]['itemName']
+    text = text.replace("eworldpartner Oguzhan Shoes ", "")
+    if "(" in text:
+        text = text.replace("(", "- Oguzhan Shoes (")
+    else:
+        text += " - Oguzhan Shoes"
+
+    print(text)
+    with open('new.json', 'r') as file:
+        data = file.readlines()
+    with open('new.json', 'w') as file:
+        data[8] = f'\t\t  "value": "{text}",\n'
+        file.writelines(data)
+
+    file = open('new.json', "r+")
+    body = json.load(file)
+    print(body)
+    resp = listing.patch_listings_item(sellerId='A2YSV8HF6GQ3SP', sku=sku, body=body,
+                                       marketplaceIds=['A1F83G8C2ARO7P'])
+    print(resp)
+    file.close()
+
+
+sku_file = open("skus.txt", "r+")
+skus = sku_file.read().splitlines()
+sku_file.close()
+
+counter = 0
+for sku in skus:
+    counter += 1
+    print(str(counter)+" out of "+str(len(skus)))
+    patch_uk(sku)
