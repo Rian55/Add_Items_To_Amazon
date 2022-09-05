020a8c56db0983dca912d4d74c19a3d788e9dcfe
Rian55 <59274842+Rian55@users.noreply.github.com>
2022-09-05 17:19:38 +0300

    adding function edited for new csv format

--- main.py
+++ main.py
@@ -82,133 +82,132 @@ def change_marketplace(fname, mktplc):
         doc = file.read()
 
     if mktplc == Marketplaces.UK:
-        doc = re.sub('("\w\w_\w\w)"', '"en_GB"', str(doc))
+        doc = re.sub('("\w\w_\w\w")', '"en_GB"', str(doc))
         doc = re.sub('("marketplace_id": "\w+")', '"marketplace_id": "A1F83G8C2ARO7P"', str(doc))
         if '"currency":' in doc:
             doc = change_price_details(doc, "GBP")
         t_code = "en"
     elif mktplc == Marketplaces.US:
-        doc = re.sub('("\w\w_\w\w)"', '"en_US"', str(doc))
+        doc = re.sub('("\w\w_\w\w")', '"en_US"', str(doc))
         doc = re.sub('("marketplace_id": "\w+")', '"marketplace_id": "ATVPDKIKX0DER"', str(doc))
         if '"currency":' in doc:
             doc = change_price_details(doc, "USD")
         t_code = "en"
     elif mktplc == Marketplaces.DE:
-        doc = re.sub('("\w\w_\w\w)"', '"de_DE"', str(doc))
+        doc = re.sub('("\w\w_\w\w")', '"de_DE"', str(doc))
         doc = re.sub('("marketplace_id": "\w+")', '"marketplace_id": "A1PA6795UKMFR9"', str(doc))
         if '"currency":' in doc:
             doc = change_price_details(doc, "EUR")
         t_code = "de"
     elif mktplc == Marketplaces.FR:
-        doc = re.sub('("\w\w_\w\w)"', '"fr_FR"', str(doc))
+        doc = re.sub('("\w\w_\w\w")', '"fr_FR"', str(doc))
         doc = re.sub('("marketplace_id": "\w+")', '"marketplace_id": "A13V1IB3VIYZZH"', str(doc))
         if '"currency":' in doc:
             doc = change_price_details(doc, "EUR")
         t_code = "fr"
     elif mktplc == Marketplaces.IT:
-        doc = re.sub('("\w\w_\w\w)"', '"it_IT"', str(doc))
+        doc = re.sub('("\w\w_\w\w")', '"it_IT"', str(doc))
         doc = re.sub('("marketplace_id": "\w+")', '"marketplace_id": "APJ6JRA9NG5V4"', str(doc))
         if '"currency":' in doc:
             doc = change_price_details(doc, "EUR")
         t_code = "it"
     elif mktplc == Marketplaces.ES:
-        doc = re.sub('("\w\w_\w\w)"', '"es_ES"', str(doc))
+        doc = re.sub('("\w\w_\w\w")', '"es_ES"', str(doc))
         doc = re.sub('("marketplace_id": "\w+")', '"marketplace_id": "A1RKKUPIHCS9HS"', str(doc))
         if '"currency":' in doc:
             doc = change_price_details(doc, "EUR")
         t_code = "es"
     elif mktplc == Marketplaces.PL:
-        doc = re.sub('("\w\w_\w\w)"', '"pl_PL"', str(doc))
+        doc = re.sub('("\w\w_\w\w")', '"pl_PL"', str(doc))
         doc = re.sub('("marketplace_id": "\w+")', '"marketplace_id": "A1C3SOZRARQ6R3"', str(doc))
         if '"currency":' in doc:
             doc = change_price_details(doc, "PLN")
         t_code = "pl"
     elif mktplc == Marketplaces.SE:
-        doc = re.sub('("\w\w_\w\w)"', '"sv_SE"', str(doc))
+        doc = re.sub('("\w\w_\w\w")', '"sv_SE"', str(doc))
         doc = re.sub('("marketplace_id": "\w+")', '"marketplace_id": "A2NODRKZP88ZB9"', str(doc))
         if '"currency":' in doc:
             doc = change_price_details(doc, "SEK")
         t_code = "sv"
     elif mktplc == Marketplaces.NL:
-        doc = re.sub('("\w\w_\w\w)"', '"nl_NL"', str(doc))
+        doc = re.sub('("\w\w_\w\w")', '"nl_NL"', str(doc))
         doc = re.sub('("marketplace_id": "\w+")', '"marketplace_id": "A1805IZSGTT6HS"', str(doc))
         if '"currency":' in doc:
             doc = change_price_details(doc, "EUR")
         t_code = "nl"
     elif mktplc == Marketplaces.AU:
-        doc = re.sub('("\w\w_\w\w)"', '"en_AU"', str(doc))
+        doc = re.sub('("\w\w_\w\w")', '"en_AU"', str(doc))
         doc = re.sub('("marketplace_id": "\w+")', '"marketplace_id": "A39IBJ37TRP1C6"', str(doc))
         if '"currency":' in doc:
             doc = change_price_details(doc, "AUD")
         t_code = "en"
     elif mktplc == Marketplaces.CA:
-        doc = re.sub('("\w\w_\w\w)"', '"en_CA"', str(doc))
+        doc = re.sub('("\w\w_\w\w")', '"en_CA"', str(doc))
         doc = re.sub('("marketplace_id": "\w+")', '"marketplace_id": "A2EUQ1WTGCTBG2"', str(doc))
         if '"currency":' in doc:
             doc = change_price_details(doc, "CAD")
         t_code = "en"
     elif mktplc == Marketplaces.MX:
-        doc = re.sub('("\w\w_\w\w)"', '"es_MX"', str(doc))
+        doc = re.sub('("\w\w_\w\w")', '"es_MX"', str(doc))
         doc = re.sub('("marketplace_id": "\w+")', '"marketplace_id": "A1AM78C64UM0Y8"', str(doc))
         if '"currency":' in doc:
             doc = change_price_details(doc, "MXN")
         t_code = "es"
     elif mktplc == Marketplaces.TR:
-        doc = re.sub('("\w\w_\w\w)"', '"tr_TR"', str(doc))
+        doc = re.sub('("\w\w_\w\w")', '"tr_TR"', str(doc))
         doc = re.sub('("marketplace_id": "\w+")', '"marketplace_id": "A33AVAJ2PDY3EV"', str(doc))
         if '"currency":' in doc:
             doc = change_price_details(doc, "TRY")
         t_code = "tr"
     elif mktplc == Marketplaces.SG:
-        doc = re.sub('("\w\w_\w\w)"', '"en_SG"', str(doc))
+        doc = re.sub('("\w\w_\w\w")', '"en_SG"', str(doc))
         doc = re.sub('("marketplace_id": "\w+")', '"marketplace_id": "A19VAU5U5O7RUS"', str(doc))
         if '"currency":' in doc:
             doc = change_price_details(doc, "SGD")
         t_code = "en"
     elif mktplc == Marketplaces.JP:
-        doc = re.sub('("\w\w_\w\w)"', '"ja_JP"', str(doc))
+        doc = re.sub('("\w\w_\w\w")', '"ja_JP"', str(doc))
         doc = re.sub('("marketplace_id": "\w+")', '"marketplace_id": "A1VC38T7YXB528"', str(doc))
         if '"currency":' in doc:
             doc = change_price_details(doc, "JPY")
         t_code = "ja"
 
-    for x in re.finditer(': "(.+)",\n\s+"language_tag"', doc):
-        translatable = x.group()[3:x.group().find('"', 4)]
-        translated = ""
-        if " - " in translatable:
-            title = translatable.split(" - ")
-            f_title = trans.translate(title[0].lower(), dest=t_code)
-            title[1] = "Saturn Ceramic"
-            translated = f_title.text + " - " + title[1]
-        else:
-            translated = trans.translate(translatable.lower(), dest=t_code).text
-        print(translated)
-        # print(translatable)
-        doc = doc.replace(translatable, translated.title())
-        print(doc)
+    # for x in re.finditer(': "(.+)",\n\s+"language_tag"', doc):
+    #     translatable = x.group()[3:x.group().find('"', 4)]
+    #     translated = ""
+    #     if " - " in translatable:
+    #         title = translatable.split(" - ")
+    #         f_title = trans.translate(title[0].lower(), dest=t_code)
+    #         translated = f_title.text + " - " + title[1]
+    #     else:
+    #         translated = trans.translate(translatable.lower(), dest=t_code).text
+    #     print(translated)
+    #     # print(translatable)
+    #     doc = doc.replace(translatable, translated.title())
+    #     print(doc)
 
-    # if t_code != "en":
-    #     doc_js = json.loads(doc)
-    #     attrs = doc_js['attributes']
-    #     for key, value in attrs.items():
-    #         for i in value:
-    #             if "language_tag" in i:
-    #                 translatable = i['value']
-    #                 translated = ""
-    #                 if " - " in translatable:
-    #                     title = translatable.split(" - ")
-    #                     f_title = trans.translate(title[0].lower(), dest=t_code)
-    #                     translated = f_title.text + " - " + title[1]
-    #                 else:
-    #                     translated = trans.translate(translatable.lower(), dest=t_code).text
-    #                 print(translated)
-    #                 # print(translatable)
-    #                 if " - " in i["value"]:
-    #                     i["value"] = translated.title()
-    #                 else:
-    #                     i["value"] = translated
-    #     doc_js['attributes'] = attrs
-    #     doc = json.dumps(doc_js, sort_keys=False, indent=2)
+    if t_code != "en":
+        doc_js = json.loads(doc)
+        attrs = doc_js['attributes']
+        for key, value in attrs.items():
+            for i in value:
+                if "language_tag" in i:
+                    translatable = i['value']
+                    translated = ""
+                    if " - " in translatable:
+                        title = translatable.split(" - ")
+                        f_title = trans.translate(title[0].lower(), dest=t_code)
+                        translated = f_title.text + " - " + title[1]
+                    else:
+                        translated = trans.translate(translatable.lower(), dest=t_code).text
+                    # print(translated)
+                    # print(translatable)
+                    if " - " in i["value"]:
+                        i["value"] = translated.title()
+                    else:
+                        i["value"] = translated
+        doc_js['attributes'] = attrs
+        doc = json.dumps(doc_js, sort_keys=False, indent=2)
 
     return doc
 
@@ -262,20 +261,21 @@ def patch_uk(sku, mktplc, f_name):
 
 
 def add_item_uk(mktplc, f_name):
+    csv_file = "aplik.csv"
     credentials, s_id = get_creds(mktplc)
     listing = ListingsItems(credentials=credentials, marketplace=mktplc)
 
-    with open('saturn_cer_sets.csv', newline='') as csvfile:
+    with open(csv_file, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
-        random = 664548553290
+        random = 825541153290
         count = 0
         for row in reader:
             sizes = []
-            for x in row['size'].split('-'):
+            for x in row['size'].split(','):
                 sizes.append(x.split("x"))
-            product = row['num']
-            item_count = int(row['items'])
-            pattern = row['pattern']
+            images = row['images'].split(",")
+            item_count = int(row['piece'])
+            colors = row['colors']
             prices = []
             for i in range(9):
                 prices.append(row[str(i)])
@@ -286,7 +286,7 @@ def add_item_uk(mktplc, f_name):
             count += 1
 
             ipd_x, ipd_y, ipd_z, max_x, max_y, max_z = 0, 0, 0, 0, 0, 0
-            for i in range(item_count):
+            for i in range(len(sizes)):
                 ipd_x += int(sizes[i][0])
                 ipd_y += int(sizes[i][1])
                 ipd_z += int(sizes[i][2])
@@ -297,50 +297,52 @@ def add_item_uk(mktplc, f_name):
                 if int(sizes[i][2]) > max_z:
                     max_z = int(sizes[i][2])
 
-            body['attributes']['item_package_dimensions'][0]['length']['value'] = ipd_x + 2
-            body['attributes']['item_package_dimensions'][0]['width']['value'] = ipd_y + 2
-            body['attributes']['item_package_dimensions'][0]['height']['value'] = ipd_z + 2
+            body['attributes']['item_package_dimensions'][0]['length']['value'] = max_x + 4
+            body['attributes']['item_package_dimensions'][0]['width']['value'] = ipd_y + 6
+            body['attributes']['item_package_dimensions'][0]['height']['value'] = ipd_z + 6
             body['attributes']['item_dimensions'][0]['height']['value'] = max_x
             body['attributes']['item_dimensions'][0]['width']['value'] = max_y
             body['attributes']['item_dimensions'][0]['length']['value'] = max_z
-            body['attributes']['item_depth_width_height'][0]['height']['value'] = max_z
-            body['attributes']['item_depth_width_height'][0]['width']['value'] = max_x
-            body['attributes']['item_depth_width_height'][0]['depth']['value'] = max_z-1
 
-            title = f"Modern Handmade Special Design {pattern} Ceramic Flowerpot Set of {item_count} - Saturn Ceramic"
-            body['attributes']['item_name'][0]['value'] = title.title()
             size_line = ""
-            for i in range(item_count):
-                size_line += sizes[i][0]+"x"+sizes[i][1]+"x"+sizes[i][2]+" cm"
-                if i != item_count-2:
+            for i in range(len(sizes)):
+                size_line += sizes[i][0] + "x" + sizes[i][1] + "x" + sizes[i][2] + " cm"
+                if i == item_count - 2:
                     size_line += " and "
-                elif i != item_count-1:
+                elif i == item_count - 1:
                     size_line = size_line
                 else:
                     size_line += ", "
-            body['attributes']['product_description'][0]['value'] = f"The dimensions of the pots are 15x15x13 cm and 20x20x18 cm respectively.They are natural earthen pots.\nThere are holes at the bottom of the pots of ceramics to prevent moisture, but there is no pot base.\nIt is 100% handmade.There may be slight differences in the same models as they are handmade.\nThey are products that will attract attention in any environment with their stylish designs, sizes and vivid colors.\nThey are special products that you can use in home or office decoration and gift to your loved ones.\nIt is recommended to wipe with a damp cloth for cleaning."
+            title = f"Lighting Bathroom Table Mirror Top Led Flute Sconce Wireless Battery Powered Daylight {size_line}, {colors} Set of {item_count}"
+            body['attributes']['item_name'][0]['value'] = title.title()
+            body['attributes']['product_description'][0]['value'] = f"<p>These sconces, which you can easily use in every area of your home, will add elegance and style to the atmosphere of your home.<br />-You will love these sconces, which do not require installation and most importantly, are battery powered and allow you to get rid of the appearance of bad cables.<br />-Easy setup.<br />-It is a decorative design.<br />-It works with battery.<br />-It is long lasting.<br />-It is economical.<br />-Does not fade and rust.<br />-Daytime.<br />- Body colors of appliques are {colors}.<br />- 3 appliques of {size_line} will be sent.</p>"
             body['attributes']['externally_assigned_product_identifier'][0]['value'] = ean13.calculate_ean(random)
             body['attributes']['number_of_items'][0]['value'] = item_count
-            body['attributes']['capacity'][0]['value'] = float("%.1f" % (max_z*max_x*max_y/1000))
-
-            img_id = "SCER-FSET-"
-            for i in range(3-len(product)):
-                img_id += "0"
-            img_id += product
-            body['attributes']['main_product_image_locator'][0]['media_location'] = f"https://seller-central-storage.s3.eu-central-1.amazonaws.com/{img_id}.jpg"
+            body['attributes']['color'][0]['value'] = colors
 
             for i in ups_codes:
                 if mktplc in ups_codes[i]:
                     body['attributes']['list_price'][0]['value_with_tax'] = float(prices[int(i)])
                     body['attributes']['purchasable_offer'][0]['our_price'][0]['schedule'][0]['value_with_tax'] = float(prices[int(i)])
 
-            with open('scer_fpot_add.json', "w", encoding="utf-8") as file:
+            with open(f_name, "w", encoding="utf-8") as file:
                 file.write(json.dumps(body, sort_keys=False, indent=2))
 
             body = json.loads(change_marketplace(f_name, mktplc))
-            # print(json.dumps(body, sort_keys=False, indent=2))
 
-            sku = "SCER-FSET-"
+            for i in range(len(images)):
+                img_id = "EWPR-APLK-"
+                for j in range(3-len(images[i])):
+                    img_id += "0"
+                img_id += images[i]
+                if i == 0:
+                    body['attributes']['main_product_image_locator'][0]['media_location'] = f"https://seller-central-storage.s3.eu-central-1.amazonaws.com/{img_id}.jpg"
+                    body['attributes']['main_product_image_locator'][0]['marketplace_id'] = body['attributes']['item_name'][0]['marketplace_id']
+                else:
+                    body['attributes'][f'other_product_image_locator_{i}'] = [{"media_location": f"https://seller-central-storage.s3.eu-central-1.amazonaws.com/{img_id}.jpg", "marketplace_id": body['attributes']['item_name'][0]['marketplace_id']}]
+
+            # print(json.dumps(body, sort_keys=False, indent=2))
+            sku = "EWPR-APLK-"
             for i in range(3 - len(str(count))):
                 sku += "0"
             sku += str(count)
@@ -353,11 +355,11 @@ def add_item_uk(mktplc, f_name):
             random += 1
 
 
-# add_item_uk(Marketplaces.DE, "scer_fpot_add.json")
+add_item_uk(Marketplaces.UK, "aplk_add.json")
 # get_attributes()
-for count in range(1, 180):
-    sku = "SCER-FPOT-"
-    for i in range(3 - len(str(count))):
-        sku += "0"
-    sku += str(count)
-    patch_uk(sku, Marketplaces.FR, "in_kw_patch.json")
+# for count in range(1, 180):
+#     sku = "SCER-FPOT-"
+#     for i in range(3 - len(str(count))):
+#         sku += "0"
+#     sku += str(count)
+#     patch_uk(sku, Marketplaces.TR, "in_kw_patch.json")
