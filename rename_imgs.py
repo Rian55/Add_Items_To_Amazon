760731202bbff4bb8314151b89ec532efc011484
Rian55 <59274842+Rian55@users.noreply.github.com>
2022-09-22 13:53:18 +0300

    image resizing added

--- rename_imgs.py
+++ rename_imgs.py
@@ -1,11 +1,20 @@
 import os
+from PIL import Image
 
-folder_name = r"C:/Users/Yenip/Downloads/wall_art/"
-for i in range(1, 1000):
+folder_name = r"C:/Users/Yenip/Downloads/Rian/"
+count = 0
+
+for path in os.listdir(folder_name):
+    count += 1
+
+for i in range(1, count+1):
     old_name = folder_name + rf"{i}.jpg"
-    new_name = folder_name + r"EWPR-WART-"
-    for j in range(3-int(len(str(i)))):
+    image = Image.open(old_name)
+    image.thumbnail((1100, 1100))
+    new_name = folder_name + r"EWPR-test-"
+    for j in range(3 - int(len(str(i)))):
         new_name += "0"
     new_name += str(i) + ".jpg"
     print(new_name)
-    os.rename(old_name, new_name)
+    image.save(new_name)
+    os.remove(old_name)
